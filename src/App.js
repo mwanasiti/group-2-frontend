9956f8aba60e3f73aaaea94eafe38d57e8ebc9d9
Anthony Kimeu <tkymeu@gmail.com>
2022-09-04 16:09:36 +0300

    made changes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import HomePage from './pages/HomePage';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/menu';
+import { PLACES } from './shared/places';
+import Greet from './components/greet';
 
-function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-      <div className="RouteSection">
-        <Header />
-        <Routes>
-          <Route exact='true' path='/' element={<HomePage />}/>
-        </Routes>
-      </div>
-      </BrowserRouter>
-    </div>
-  );
+class App extends React.Component {					
+  
+	constructor(props){
+		super(props);
+		
+		this.state = {
+			places: PLACES
+		};
+	}
+	
+	render(){
+		return (
+			<div className='App'>
+				<Navbar color="primary">
+					<div className="container" id="brand">
+						<NavbarBrand>Incredible India</NavbarBrand>
+					</div>
+				</Navbar>
+				<Greet />
+				<h1>Famous Places of India</h1>
+				<Menu places={this.state.places} />
+			</div>
+		);
+	}
 }
 
 export default App;
